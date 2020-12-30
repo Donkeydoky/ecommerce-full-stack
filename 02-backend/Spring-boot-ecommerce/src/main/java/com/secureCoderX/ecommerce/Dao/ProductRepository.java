0c8562ac05a0a86c1c155c429f9374fd55148848
OscarXu <15222875592@163.com>
2020-12-30 02:27:25 -0500

    Revert "#006 v_1.0 CDI front End"
    This reverts commit 0e22c7086317de394353e13d3d1e3630b200f32c.

--- 02-backend/Spring-boot-ecommerce/src/main/java/com/secureCoderX/ecommerce/Dao/ProductRepository.java
+++ 02-backend/Spring-boot-ecommerce/src/main/java/com/secureCoderX/ecommerce/Dao/ProductRepository.java
@@ -2,8 +2,6 @@ package com.secureCoderX.ecommerce.Dao;
 
 import com.secureCoderX.ecommerce.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/common/product';
-import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,21 +7,9 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  //initialize product list array
-  products: Product[] = [];
-
-  constructor(private productService: ProductService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.listProducts();
-  }
-
-  listProducts() {
-    this.productService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    );
   }
 
 }
