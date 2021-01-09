8b02a4e78940e89642684d6412641bdc545f69ae
OscarXu <15222875592@163.com>
2021-01-09 01:37:36 -0500

    #011 v.2.0.2 adding sider bar functionnality: integration
    6. Modify Spring Boot app - REST Repository needs new method
    7. Update Angular Service to call new URL on Spring Boot app

--- 02-backend/Spring-boot-ecommerce/src/main/java/com/secureCoderX/ecommerce/Dao/ProductRepository.java
+++ 02-backend/Spring-boot-ecommerce/src/main/java/com/secureCoderX/ecommerce/Dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.secureCoderX.ecommerce.Dao;
 
 import com.secureCoderX.ecommerce.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //SELECT * FROM product where category_id = ?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
 }
